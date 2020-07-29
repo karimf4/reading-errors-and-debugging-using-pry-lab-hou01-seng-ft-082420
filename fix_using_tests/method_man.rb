350d5f320fd4004ce3fc02d96478d1461ed94986
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-07-29 04:17:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  player1 = "Rocky"
-  player2 = "Bullwinkle" 
+  player1 == Rocky
+  player2 == Bullwinkle
   "Hello #{player1} & #{playar2}"
 end
 
